b797c029b6cfc9492a169626520bbba6cddd6f05
satananastyle <scorpioshassol@outlook.com>
2019-06-27 14:13:53 +0300

    Улучшает код
    Оборачиваем циклы в фунции, убираем ненужные, дублирующиеся функции

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var comments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -18,29 +19,28 @@ var names = [
   'Бернадед'
 ];
 
-var random = function (length) {
-  return Math.floor(Math.random() * length);
+var getRandomInRange = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function getRandomInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+var getPictures = function () {
+  var photos = [];
+  for (var i = 1; i <= 25; i++) {
+    var picture = {
+      url: 'photos/' + i + '.jpg',
+      likes: getRandomInRange(15, 200),
+      comment: [{
+        avatar: 'img/avatar-' + getRandomInRange(1, 6) + '.svg',
+        message: comments[getRandomInRange(1, comments.length)],
+        name: names[getRandomInRange(1, names.length)],
+      }]
+    };
+    photos.push(picture);
+  }
+  return photos;
+};
 
-var pictures = [];
-
-for (var i = 0; i < 25; i++) {
-  var picture = {
-    url: 'photos/' + (i + 1) + '.jpg',
-    likes: getRandomInRange(15, 200),
-    comment: [{
-      avatar: 'img/avatar-' + getRandomInRange(1, 6) + '.svg',
-      message: comments[random(comments.length)],
-      name: names[random(names.length)],
-    }]
-  };
-
-  pictures[i] = picture;
-}
+var pictures = getPictures();
 
 var listElement = document.querySelector('.pictures');
 
@@ -58,9 +58,12 @@ var renderPicture = function (photo) {
   return pictureElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var j = 0; j < pictures.length; j++) {
-  fragment.appendChild(renderPicture(pictures[j]));
-}
+var renderPictures = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < pictures.length; i++) {
+    fragment.appendChild(renderPicture(pictures[i]));
+    listElement.appendChild(fragment);
+  }
+};
 
-listElement.appendChild(fragment);
+renderPictures();
